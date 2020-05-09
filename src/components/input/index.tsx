391723f69e7f8b07ec39861e2479ed9c8adf25e1
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-05-09 19:12:17 -0300

    Crated input and button components

---
+++ src/components/input/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput, Icon } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+}
+
+const input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
+  <Container>
+    <Icon name={icon} size={360} color="#666360" />
+    <TextInput
+      keyboardAppearance="dark"
+      placeholderTextColor="#666360"
+      {...rest}
+    />
+  </Container>
+);
+
+export default input;
