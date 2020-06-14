e52770a0593d1ff4454dfa9f21e1cc023974f2d5
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-06-14 14:27:02 -0300

    Created pages and routes of profile and appointments

---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => {
+  return <View style={{ flex: 1, justifyContent: 'center' }} />;
+};
+
+export default CreateAppointment;
---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Profile: React.FC = () => {
+  return <View style={{ flex: 1, justifyContent: 'center' }} />;
+};
+
+export default Profile;
