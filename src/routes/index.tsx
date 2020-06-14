6feb246a6298fc66a0131e050a3e93bd45d10f82
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-06-14 14:57:04 -0300

    Created profile page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { View, ActivityIndicator } from 'react-native';
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
-import { useAuth } from '../hooks/Auth';
+import { useAuth } from '../hooks/auth';
 
 const Routes: React.FC = () => {
   const { user, loading } = useAuth();
